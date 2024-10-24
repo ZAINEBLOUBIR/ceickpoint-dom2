7cbefc13b33f8d8b627e8d9fad92437f484525f6
zeineb <loubirizeineb@gmail.com>
2024-10-24 22:13:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var colorBox = document.getElementById('color-box');
+    var changeColorBtn = document.getElementById('change-color-btn');
+
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    changeColorBtn.addEventListener('click', () => {
+        var randomColor = getRandomColor();
+        colorBox.style.backgroundColor = randomColor;
+    });
+});
